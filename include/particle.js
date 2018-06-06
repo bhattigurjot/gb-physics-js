2504601c2b9b95f21138186f61f2c23fa31a1d19
bhattigurjot <bhattigurjot@gmail.com>
2018-06-06 15:34:33 -0600

    particle class updated to unclude position, velocity and acc along with integrate function

--- include/particle.js
+++ include/particle.js
@@ -1,9 +1,32 @@
 class Particle {
     constructor(_x, _y, _z, _r) {
-        this.x = _x;
-        this.y = _y;
-        this.z = _z;
-        this.r = _r;
+        this.position = new Vector3D(_x,_y,_z);        
+        this.velocity = new Vector3D(0,0,0);
+        this.acceleration = new Vector3D(0,0,0);
+        this.radius = _r;
+
+        this.damping = 0.0;
+        this.inverseMass = 0.0;
+    }
+
+    integrate(duration) {
+        // No integration because of Infinite Mass
+        if (this.inverseMass <= 0.0) {
+            return;
+        }
+
+        // Update position
+        this.position.addScaledVector(this.velocity, duration);
+        
+        // Calculate acceleration
+        let resultAcc = acceleration;
+        // let resultAcc = new Vector3D();
+
+        // Update velocity
+        this.velocity.addScaledVector(resultAcc, duration);
+
+        // Add drag to velocity
+        this.velocity *= Math.pow(this.damping, duration);
     }
 
     draw() {
