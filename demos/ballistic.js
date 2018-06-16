15fa62a9b6a19d570ec98369482d8321a5e8c833
bhattigurjot <bhattigurjot@gmail.com>
2018-06-16 14:09:20 -0600

    Add p5 canvas to ballistic to make it standalone example

--- demos/ballistic.js
+++ demos/ballistic.js
@@ -1,3 +1,30 @@
+// P5 CANVAS
+
+let b = new Ballistic();
+
+let canvas;
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+    background(50);
+}
+
+function draw() {
+    background(50);
+    b.update();
+    b.display();
+}
+
+function keyPressed() {
+    b.keyPressed(keyCode);
+}
+
+function mouseClicked() {
+    b.mouseClicked();
+}
+
+// BALLISTICS
+
 let ShotType = Object.freeze({
     "UNUSED":0,
     "PISTOL":1,
